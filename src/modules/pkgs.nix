c9716cc9ce6afb78ebb1d271acbb75f41030b5a3
Gabriel Guerra <gfranklings@gmail.com>
2024-10-18 19:13:23 -0400

    chore: Installing github-cli globally

--- src/modules/pkgs.nix
+++ src/modules/pkgs.nix
@@ -6,6 +6,7 @@
 
     systemPackages = with pkgs; [
       git
+      github-cli
       vim
       emacs
       vscode.fhs
--- src/modules/users.nix
+++ src/modules/users.nix
@@ -1,10 +1,7 @@
-{ pkgs, ... }: {
+{
   users.users.alpha = {
     isNormalUser = true;
     initialPassword = "alpha123.";
     extraGroups = ["wheel" "audio" "video" "vboxusers"];
-    packages = with pkgs; [
-      github-cli
-    ];
   };
 }
